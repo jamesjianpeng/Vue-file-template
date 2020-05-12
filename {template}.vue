d5fe2f58dc7d43266cfb681227cb06220d50b4ca
v_jianjpeng(彭涧) <v_jianjpeng@wesure.cn>
2020-05-12 19:09:44 +0800

    feat: template 初步完成

---
+++ {template}.vue
@@ -0,0 +1,29 @@
+<style lang="style" scope>
+.msg {
+  font-size: 30px;
+}
+</style>
+
+<template>
+  <div class="msg">
+    {{ msg }}
+  </div>
+</template>>
+
+<script>
+export default {
+  name: "{template}",
+  data () {
+    msg: 'hello!'
+  },
+  created () {
+    console.log('created')
+  },
+  mounted () {
+    console.log('mounted')
+  },
+  beforeDestroy () {
+    console.log('beforeDestroy')
+  }
+}
+</script>
